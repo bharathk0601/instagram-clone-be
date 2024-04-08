f1abedad8bec1c25f4a3102df58b6bd2617459c4
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-08 22:35:35 +0530

    enums

---
+++ src/shared/enums/enum.ts
@@ -0,0 +1,16 @@
+export enum Environment {
+  development = 'development',
+  staging = 'staging',
+  production = 'production',
+}
+
+export enum Econfig {
+  ENVIRONMENT = 'ENVIRONMENT',
+  POSTGRES_HOST = 'POSTGRES_HOST',
+  POSTGRES_PORT = 'POSTGRES_PORT',
+  POSTGRES_USER = 'POSTGRES_USER',
+  POSTGRES_PASSWORD = 'POSTGRES_PASSWORD',
+  POSTGRES_DATABASE = 'POSTGRES_DATABASE',
+  THROTTLE_LIMIT = 'THROTTLE_LIMIT',
+  THROTTLE_TTL = 'THROTTLE_TTL',
+}
