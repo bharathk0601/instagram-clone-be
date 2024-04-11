c2f6a0543e06df5949ae0fd4084cd071ec0a15db
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-11 10:12:53 +0530

    disbaled logger color for higher environment

--- src/shared/logger/winston.config.ts
+++ src/shared/logger/winston.config.ts
@@ -1,3 +1,4 @@
+import config from '@/config/config';
 import * as winston from 'winston';
 const { splat, combine, timestamp, printf, colorize } = winston.format;
 
@@ -5,19 +6,19 @@ const myFormat = printf(({ timestamp, level, message }) => {
   return `${timestamp} | ${level} | ${message}`;
 });
 
-const timezoned = () =>
-  new Date().toLocaleString('en-US', {
+const timezoned = () => {
+  return new Date().toLocaleString('en-US', {
     timeZone: 'Asia/Calcutta',
   });
+};
+
+const format = [timestamp({ format: timezoned }), splat()];
+if (config.isDev()) {
+  format.push(colorize({ level: true }));
+}
+format.push(myFormat);
 
 export const logger = winston.createLogger({
-  format: combine(
-    timestamp({ format: timezoned }),
-    splat(),
-    colorize({
-      level: true,
-    }),
-    myFormat,
-  ),
+  format: combine(...format),
   transports: [new winston.transports.Console({ level: 'debug' })],
 });
