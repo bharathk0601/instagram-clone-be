43252769e55d24163c3a23ee253b5cef417de068
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-11 15:03:28 +0530

    added cloudinary config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,19 @@ import logger from '@/shared/logger/logger';
 
 class Config {
   private readonly ENVIRONMENT: string = process.env.ENVIRONMENT;
+
   private readonly POSTGRES_HOST: string = process.env.POSTGRES_HOST;
   private readonly POSTGRES_PORT: number = Number(process.env.POSTGRES_PORT);
   private readonly POSTGRES_USER: string = process.env.POSTGRES_USER;
   private readonly POSTGRES_PASSWORD: string = process.env.POSTGRES_PASSWORD;
   private readonly POSTGRES_DATABASE: string = process.env.POSTGRES_DATABASE;
+
   private readonly THROTTLE_LIMIT: number = 20;
   private readonly THROTTLE_TTL: number = 1000;
+
+  private readonly CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+  private readonly CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+  private readonly CLOUD_NAME = process.env.CLOUD_NAME;
 
   constructor() {
     switch (this.ENVIRONMENT) {
