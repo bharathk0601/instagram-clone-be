e7d619c150700e98402d2049c0448d460b47f71a
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-11 15:53:03 +0530

    cloudinary config

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -4,5 +4,5 @@ import config from '@/config/config';
 export const cloudinaryConfig: ConfigOptions = {
   cloud_name: config.get('CLOUD_NAME'),
   api_key: config.get('CLOUDINARY_API_KEY'),
-  api_secret: config.get('CLOUD_NAME'),
+  api_secret: config.get('CLOUDINARY_API_SECRET'),
 };
