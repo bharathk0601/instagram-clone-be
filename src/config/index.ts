7a61bdacfb40a89aef85f2b1c6aec1603a2bc222
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-13 11:21:21 +0530

    created throttler config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,4 @@ export * from './config';
 export * from './orm-config';
 export * from './cloudinary.config';
 export * from './redis.config';
+export * from './throttler.config';
---
+++ src/config/throttler.config.ts
@@ -0,0 +1,6 @@
+import { ThrottlerModuleOptions } from '@nestjs/throttler';
+import { config } from './config';
+
+export const ThrottleConfig: ThrottlerModuleOptions = {
+  throttlers: [{ ttl: config.get('THROTTLE_TTL'), limit: config.get('THROTTLE_LIMIT') }],
+};
