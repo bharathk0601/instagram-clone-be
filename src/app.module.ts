77b5e5ff3cfa2c0553b090c3645044ad5887c873
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-17 23:28:35 +0530

    app module changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,28 +3,27 @@ import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { DbConfig } from '@/config';
-import { config } from '@/config';
+import { DbConfig, ThrottleConfig } from '@/config';
 
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './modules/user/auth.module';
 import { PostModule } from './modules/post/post.module';
 import { LikeModule } from './modules/like/like.module';
 import { CommentModule } from './modules/comment/comment.module';
 import { StatusModule } from './modules/status/status.module';
 import { SharedModule } from './modules/shared/shared.module';
+import { RepositoryModule } from './modules/repository/repository.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(DbConfig),
-    ThrottlerModule.forRoot({
-      throttlers: [{ ttl: config.get('THROTTLE_TTL'), limit: config.get('THROTTLE_LIMIT') }],
-    }),
+    ThrottlerModule.forRoot(ThrottleConfig),
     UserModule,
     PostModule,
     LikeModule,
     CommentModule,
     StatusModule,
     SharedModule,
+    RepositoryModule,
   ],
   controllers: [],
   providers: [
