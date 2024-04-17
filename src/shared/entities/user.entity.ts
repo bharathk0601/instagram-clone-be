a279d6b9764790b6a0fbabe2f99ccccafcbfebad
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-17 23:31:19 +0530

    user entity

--- src/shared/entities/user.entity.ts
+++ src/shared/entities/user.entity.ts
@@ -1,10 +1,33 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, DeepPartial, PrimaryColumn } from 'typeorm';
 
-@Entity({ name: 'users' })
-export class UserEntity {
-  @Column({ name: 'id' })
-  id: number;
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryColumn({ name: 'user_id' })
+  userId: string;
 
-  @Column({ name: 'user_name' })
-  userName: string;
+  @Column({ name: 'email' })
+  email: string;
+
+  @Column({ name: 'password', select: false })
+  password: string;
+
+  @Column({ name: 'is_verified' })
+  isVerified: boolean;
+
+  @Column({ name: 'is_active' })
+  isActive: boolean;
+
+  @Column({ name: 'is_deleted' })
+  isDeleted: boolean;
+
+  @Column({ name: 'deactivated_at' })
+  deactivatedAt: Date;
+
+  @Column({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column({ name: 'updated_at' })
+  updatedAt: Date;
 }
+
+export type TUser = DeepPartial<User>;
