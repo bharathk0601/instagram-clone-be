644e1598eb0387d0e78084fb01d278c7a4e756ae
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-17 23:32:16 +0530

    APP_CONSTANTS

---
+++ src/shared/constants/constant.ts
@@ -0,0 +1,5 @@
+export const APP_CONSTANTS = {
+  USER_ID_LEN: 10,
+  OTP_EXPIRY_TIME: 30 * 60 * 1000, // 30 mins
+  PASSWORD_TOKEN_EXPIRY_TIME: 30 * 60 * 1000, // 30 mins
+};
--- src/shared/dtos/index.ts
+++ src/shared/dtos/index.ts
@@ -1,2 +1,4 @@
 export * from './test.dto';
 export * from './error-res.dto';
+export * from './auth.dto';
+export * from './common.dto';
