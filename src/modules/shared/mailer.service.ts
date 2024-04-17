ab8cf07355d05a3bede7e9922e5509eaff1b2262
bharath kumar s <bharathkumar.s@niveussolutions.com>
2024-04-17 23:33:58 +0530

    verifyEmail and restPasswordToken method implementation

--- src/modules/shared/mailer.service.ts
+++ src/modules/shared/mailer.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as kickbox from 'kickbox';
 import * as nodemailer from 'nodemailer';
+import * as fs from 'fs';
 
 import { config } from '@/config';
 import { NodeMailerConfig } from '@/config/mailer.config';
 import { MailOptions } from '@/shared/interfaces';
+import { Utils } from '@/shared/utils';
 
 @Injectable()
 export class MailerService implements OnModuleInit {
@@ -19,9 +21,10 @@ export class MailerService implements OnModuleInit {
   /**
    *
    * @param {string} email
+   * @param {ReqCtx} ctx
    * @returns {boolean}
    */
-  public async isEmailValid(email: string): Promise<boolean> {
+  public async isEmailValid(email: string, ctx: ReqCtx): Promise<boolean> {
     if (config.get('BYPASS_EMAIL_EXISTENCE_CHECK')) {
       return true;
     }
@@ -30,6 +33,7 @@ export class MailerService implements OnModuleInit {
       this.kickBox.verify(email, {}, (err, res) => {
         if (err) return reject(err);
 
+        ctx.logger.log(`res | ${JSON.stringify(res)}`, 'MailerService | isEmailValid');
         resolve(res?.body?.result === 'deliverable');
       });
     });
@@ -50,5 +54,34 @@ export class MailerService implements OnModuleInit {
         return resolve(info.response);
       });
     });
+  }
+
+  /**
+   *
+   * @param {string} toEmail
+   * @param {string} otp
+   */
+  public async verifyEmail(toEmail: string, otp: string) {
+    const html = fs.readFileSync('src/templates/verify-email.html', { encoding: 'utf-8' });
+    const template = Utils.htmlValSubstituter(html, { otp });
+    const mailOptions = {
+      to: toEmail,
+      subject: 'Email Verfication OTP',
+      html: template,
+    };
+
+    await this.sendEmail(mailOptions);
+  }
+
+  public async restPasswordToken(toEmail: string, tokenUrl: string) {
+    const html = fs.readFileSync('src/templates/reset-password.html', { encoding: 'utf-8' });
+    const template = Utils.htmlValSubstituter(html, { tokenUrl });
+    const mailOptions = {
+      to: toEmail,
+      subject: 'Reset Password',
+      html: template,
+    };
+
+    await this.sendEmail(mailOptions);
   }
 }
